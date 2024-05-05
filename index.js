feb5d60e24740f58ad99aba40675129b9e6e9082
harshit0x0 <hpandey8888@gmail.com>
2024-05-05 20:34:30 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+const dbURL = process.env.dbURL;
+mongoose.connect(dbURL);
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("database connected");
+})
+
+const dataSchema = new mongoose.Schema({
+    date: Date,
+    hours: Number,
+    limit: Number
+})
+
+const Data = mongoose.model('Data', dataSchema);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT} with cors enabled`);
+})
+
+app.get('/api', async (req, res) => {
+    const results = await Data.find();
+    res.json(results);
+})
+
+app.get('/', async (req, res) => {
+    const results = await Data.find();
+    res.send(results);
+})
+
+app.post('/send', async (req, res) => {
+    const newData = new Data(req.body);
+    await newData.save();
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.post('/update', async (req, res) => {
+    await Data.findOneAndDelete({ _id: req.body.id });
+    console.log(req.body.id);
+    res.redirect('/');
+})
